2a5ced249423a35f873142f7fd78597b96314074
Lauren <llwor94@gmail.com>
2018-12-18 18:45:57 -0800

    file structure set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+		box-sizing: border-box;
+  }
+
+	html, body {
+		height: 100%;
+		width: 100%;
+	}
+`;
 
 class App extends Component {
 	render() {
 		return (
 			<div>
+				<GlobalStyle />
 				<p>Hello World</p>
 			</div>
 		);
