5f586830e4c8cafa154193a660d9101caaa98187
Lauren <llwor94@gmail.com>
2019-01-03 16:25:15 -0800

    topics reducer

--- client/src/store/reducers/quizReducer.js
+++ client/src/store/reducers/quizReducer.js
@@ -8,6 +8,9 @@ import {
 	UPDATE_USER_SCORE_REQUEST,
 	UPDATE_USER_SCORE_FAILURE,
 	UPDATE_USER_SCORE_SUCCESS,
+	FETCH_TOPICS_REQUEST,
+	FETCH_TOPICS_SUCCESS,
+	FETCH_TOPICS_FAILURE,
 } from '../actions';
 
 const initialState = {
@@ -15,6 +18,7 @@ const initialState = {
 	quiz: undefined,
 	loading: false,
 	error: undefined,
+	topics: undefined,
 };
 
 const quizReducer = (state = initialState, { payload, type }) => {
@@ -66,6 +70,23 @@ const quizReducer = (state = initialState, { payload, type }) => {
 				loading: false,
 			};
 		case UPDATE_USER_SCORE_FAILURE:
+			return {
+				...state,
+				loading: false,
+				error: payload,
+			};
+		case FETCH_TOPICS_REQUEST:
+			return {
+				...state,
+				loading: true,
+			};
+		case FETCH_TOPICS_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				topics: payload,
+			};
+		case FETCH_TOPICS_FAILURE:
 			return {
 				...state,
 				loading: false,
