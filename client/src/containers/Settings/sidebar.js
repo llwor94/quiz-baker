b20b6768d1eaa64c2fc6d19b49799a1cab9fd4b0
Trevor Fehrman <trevorfehrman@gmail.com>
2019-01-15 01:22:37 -0800

    button issues

--- client/src/containers/Settings/sidebar.js
+++ client/src/containers/Settings/sidebar.js
@@ -1,44 +1,61 @@
-import React, { useEffect, useState, Fragment } from 'react';
-import { connect } from 'react-redux';
-import server from '../../utils/server';
+import React, { useEffect, useState, Fragment } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
 
-import { getUser } from '../../store/actions/authActions';
-import ImageUpload from '../ImageUpload';
-import Button from '../../components/Styles/Button';
-import { LargeImage } from '../../components/Styles/Image';
-import UpdateUsername from './updateUsername';
+import server from "../../utils/server";
+import { getUser } from "../../store/actions/authActions";
+import ImageUpload from "../ImageUpload";
+import Button from "../../components/Styles/Button";
+import { LargeImage } from "../../components/Styles/Image";
+import UpdateUsername from "./updateUsername";
+
+const ProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  left: 0;
+  top: 0;
+`;
+
 
 const Sidebar = ({ user, getUser, ...props }) => {
-	const [ imageUpdate, setImageUpdate ] = useState(false);
+  const [imageUpdate, setImageUpdate] = useState(false);
 
-	useEffect(() => {
-		getUser(user.id);
-	}, []);
-	if (imageUpdate)
-		return (
-			<ImageUpload doneEditting={() => setImageUpdate(false)} {...props}>
-				<Button
-					icon='pi pi-arrow-left'
-					label='back'
-					onClick={() => setImageUpdate(false)}
-				/>
-			</ImageUpload>
-		);
-	else
-		return (
-			<div style={{ display: 'flex', flexDirection: 'column', position: 'absolute', left: 0, top: 0, borderRight: '1px solid black', height: '100%' }}>
-				<div>Welcome, {user.username}!</div>
+  useEffect(() => {
+    getUser(user.id);
+  }, []);
+  if (imageUpdate)
+    return (
+      <ImageUpload doneEditting={() => setImageUpdate(false)} {...props}>
+        <Button
+		secondary
+          icon="pi pi-arrow-left"
+          label="back"
+          onClick={() => setImageUpdate(false)}
+        />
+      </ImageUpload>
+    );
+  else
+    return (
+      <ProfileWrapper>
+        <LargeImage src={user.img_url} />
+        <h4>Welcome, {user.username}!</h4>
+        <div style={{display: "flex"}}>
+        <Button secondary label="Update Image?" onClick={() => setImageUpdate(true)} />
 
-				<LargeImage src={user.img_url} />
-				<Button label='Update Image?' onClick={() => setImageUpdate(true)} />
-
-				<UpdateUsername />
-			</div>
-		);
+        <UpdateUsername />
+        </div>
+      </ProfileWrapper>
+    );
 };
 
 const mapStateToProps = ({ authReducer }) => ({
-	user: authReducer.user,
+  user: authReducer.user
 });
 
-export default connect(mapStateToProps, { getUser })(Sidebar);
+export default connect(
+  mapStateToProps,
+  { getUser }
+)(Sidebar);
