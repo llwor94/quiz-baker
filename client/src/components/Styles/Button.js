c7417aa6b777a918ecaeed88b988664c4e2e759d
Lauren <llwor94@gmail.com>
2019-01-15 01:11:54 -0800

    updated button styles

--- client/src/components/Styles/Button.js
+++ client/src/components/Styles/Button.js
@@ -5,23 +5,25 @@ import { Button as StyledButton } from 'primereact/button';
 const StyledButtonWrapper = styled.div`
 	display: inline-block;
 	width: ${props => props.full && '100%'};
-	
+
 	.p-button {
 		width: ${props => props.full && '100%'};
-		background-color: ${props => props.theme.pink};
-		border-color: ${props => props.theme.pink};
+		background-color: ${props => (props.secondary ? props.theme.aqua : props.theme.pink)};
+		border-color: ${props => (props.secondary ? props.theme.aqua : props.theme.pink)};
 		&:enabled:hover {
-			background-color: ${props => props.theme.darkPink};
-			border-color: ${props => props.theme.darkPink};
+			background-color: ${props =>
+				props.secondary ? props.theme.aqua : props.theme.darkPink};
+			border-color: ${props => (props.secondary ? props.theme.aqua : props.theme.darkPink)};
 		}
 		&:enabled:focus {
-			box-shadow: 0 0 0 0.2em ${props => props.theme.darkPink};
+			box-shadow: 0 0 0 0.2em
+				${props => (props.secondary ? props.theme.aqua : props.theme.darkPink)};
 		}
 	}
 `;
 
-const Button = ({ label, onClick, disabled, icon, style, full }) => (
-	<StyledButtonWrapper full={full} style={style}>
+const Button = ({ label, onClick, disabled, icon, style, full, secondary }) => (
+	<StyledButtonWrapper full={full} style={style} secondary={secondary}>
 		<StyledButton label={label} onClick={onClick} disabled={disabled} icon={icon} />
 	</StyledButtonWrapper>
 );
