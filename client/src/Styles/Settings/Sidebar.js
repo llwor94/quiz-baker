8d189c1e62c244030744fede4c0ec28dfbbcac52
Trevor Fehrman <trevorfehrman@gmail.com>
2019-01-17 17:58:58 -0800

    styling changes

--- client/src/Styles/Settings/Sidebar.js
+++ client/src/Styles/Settings/Sidebar.js
@@ -3,13 +3,16 @@ import styled from 'styled-components';
 export const ProfileWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+	justify-content: flex-start;
+	align-items: flex-start;
+	position: absolute;
+	top: 0;
+	left: 0;
 `;
 
 export const ProfileButtonWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+	justify-content: flex-start;
+	align-items: flex-start;
 `;
