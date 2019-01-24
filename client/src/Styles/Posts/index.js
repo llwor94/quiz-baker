aadf07172b503988fdded2d7ccfd01cf67d87376
Lauren <llwor94@gmail.com>
2019-01-23 17:12:36 -0800

    image tweaks

--- client/src/Styles/Posts/index.js
+++ client/src/Styles/Posts/index.js
@@ -5,21 +5,29 @@ export const Wrapper = styled.div`
 	display: flex;
 	margin: 70px 0;
 	justify-content: stretch;
-
-	.quizBaker {
-		filter: grayscale(24%);
-		height: 400px;
-		position: fixed;
-		right: 300px;
-		top: 325px;
-		opacity: .8;
-	}
 `;
 export const CommentsWrapper = styled.div`
 	flex-grow: 1;
 	position: relative;
 	margin-left: 10px;
 	height: calc(100vh - 100px);
+	.image {
+		position: fixed;
+		top: 59px;
+		width: 32%;
+		padding: 60px;
+		padding-top: 150px;
+		min-width: 500px;
+		.quizBaker {
+			filter: grayscale(24%);
+			height: 400px;
+
+			/* position: fixed;
+		right: 300px;
+		top: 325px; */
+			opacity: .8;
+		}
+	}
 `;
 export const InnerWrapper = styled.div`
 	position: fixed;
@@ -28,6 +36,7 @@ export const InnerWrapper = styled.div`
 	border-right: 1px solid ${props => props.theme.pink};
 	border-left: 1px solid ${props => props.theme.pink};
 	top: 59px;
+	min-width: 500px;
 	width: 32%;
 	background-color: ${props => props.comments && props.theme.secondary};
 	margin: 0;
