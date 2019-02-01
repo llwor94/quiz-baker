837b237d19e1c11f0f649261d514138364d7779f
Lauren <llwor94@gmail.com>
2019-02-01 02:52:45 -0800

    pages cleaned up

--- client/src/pages/Quizzes.js
+++ client/src/pages/Quizzes.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, createContext } from 'react';
+import React, { useState, createContext } from 'react';
 
 import QuizzesContainer from '../containers/Quizzes';
 
--- client/src/pages/UserQuiz.js
+++ client/src/pages/UserQuiz.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState, createContext, useContext } from 'react';
+import React, { useState, createContext, useContext } from 'react';
+
 import UserQuizContainer from '../containers/UserQuiz';
 import { AuthCtx } from '../Auth';
 import Loading from '../components/Loading';
+
 export const UserQuizCtx = createContext([ undefined, () => {} ]);
 export const QuizQuestionsCtx = createContext([ undefined, () => {} ]);
+
 const UserQuiz = props => {
 	const [ quiz, setQuiz ] = useState(undefined);
 	const [ questions, setQuestions ] = useState(undefined);
 	const { user } = useContext(AuthCtx);
+
 	if (!user) return <Loading />;
 	else
 		return (
